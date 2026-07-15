a3ec501ecbcf8c5dde9e5bd05b5c626b1e1ed51f
Alvaro <telecom25a@gmail.com>
2026-07-15 12:15:47 -0400

    Fix: Se arreglo problema con conexion con la base de datos

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import { setServers } from "node:dns";
 import { env } from "../config/env";
 
-
+// Fuerza a Node.js a utilizar DNS compatibles con MongoDB Atlas
+setServers(["8.8.8.8", "1.1.1.1"]);
 export const connectDB = async () => {
   try {
     await mongoose.connect(env.mongoUri);
